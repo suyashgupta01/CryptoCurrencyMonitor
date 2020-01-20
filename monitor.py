e41cc9afb6ef29d13e0c548b22590a2650567785
suyash gupta <suyashgupta0001@gmail.com>
2020-01-20 23:40:17 +0530

    first commit

---
+++ monitor.py
@@ -0,0 +1,29 @@
+import requests as rq
+import bs4 
+import time 
+inp = input("Select The cryptocurrency that you want to monitor?\n1) BTC\n2) ETH\n3) XRP\n4) LTC\n")
+if inp == '1':
+    url = "https://cointelegraph.com/bitcoin-price-index"
+    cryptocurrency = "Bitcoin"
+elif inp == '2':
+    url = "https://cointelegraph.com/ethereum-price-index"
+    cryptocurrency = "Etherium"
+elif inp == '3':
+    url = "https://cointelegraph.com/xrp-price-index"
+    cryptocurrency = "Ripple"
+elif inp == '4':
+    url = "https://cointelegraph.com/ltc-price-index"
+    cryptocurrency = "Litecoin"
+    
+update_time = int(input("Update " + cryptocurrency + "\'s price in every ? seconds: "))
+total_time = int(input("Monitor " + cryptocurrency + "\'s price for ? seconds: "))
+headerss = {'User-Agent': 'Guptaji\'s_request'}
+
+iterations = int(update_time/ total_time)
+i = 0
+while i <= iterations:
+    r = rq.get(url, headers = headerss)
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    with open("BTC_price_record.txt", "a+") as f:
+        f.write(soup.find_all('div', attrs = {'class' : 'price-value'})[0].get_text() + "\n")
+    time.sleep(update_time)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Realtime Crypto currency monitor
+
+This python script monitors the price of any of the given crypto currencies.
+The prices are updated in a fixed time period (given by user) for a fixed duration (also given by user). The data is stored in a .txt file.
+
+
+Note:
+	website used to scrap data: cointelegraph.com.
